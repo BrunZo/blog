c9bac87b7a27eebbe02420cfdfdd0c3ec21207a4
BrunZo <ziger.bruno@gmail.com>
2025-10-18 00:38:39 -0300

    add: working on...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,32 @@ export default function HomePage() {
       </p>
       
       <h2 className='font-bold text-xl mt-4'>working on...</h2>
+      <table className='border-separate border-spacing-x-4'>
+        <tbody>
+          <tr>
+            <td>
+              <a href='https://servilleta.vercel.app/' className='text-md text-gray-500 hover:text-gray-800'>Servilleta</a>
+            </td>
+            <td>A site for my math notes, currently in development.</td>
+          </tr>
+          <tr>
+            <td>
+              <a href='https://iberoofficial.vercel.app/' className='text-md text-gray-500 hover:text-gray-800'>ibero-official</a>
+            </td>
+            <td>Resultados históricos de la Olimpiada Iberoamericana de Matemática.</td>
+          </tr>
+          <tr>
+            <td>
+              <a href='/' className='text-md text-gray-500 hover:text-gray-800'>
+                this
+              </a>
+            </td>
+            <td>
+              Look around.
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
