b35d18051d10f977d79656b870e47bd58c424c7e
BrunZo <ziger.bruno@gmail.com>
2025-10-26 14:24:51 -0300

    fix: add styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import 'zettel/ui/styles/global.css';
 import Navbar from "@/ui/navbar";
+import "@/ui/global.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
