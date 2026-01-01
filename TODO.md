c606ebb40657f4c97724605e6872698a98e12a27
BrunZo <ziger.bruno@gmail.com>
2026-01-01 15:28:18 -0300

    Update package dependencies and enhance note versioning features. Added simple-git for Git operations and improved the note page to support version selection and diff viewing. Updated subproject commits for notes and zettel.

---
+++ TODO.md
@@ -0,0 +1,34 @@
+# TODO
+
+## Bugs:
+
+- Tags on other pages not showing
+- Dateless notes showing despite date filtering
+- Move date filter
+- Allow for LaTeX in .tsx
+
+## More changes
+
+Convert about me into .tsx
+
+## Component for "books"
+
+The same way you can create a zettel grid, have a component which allows viewing one zettel after another, just as in a "book", where each zettel is a small paragraph. Also, chapter, index, etc.
+
+## Definitions / theorems in different zettels
+
+Each definition / theorem should be part of a different zettel. Having a very sparse text-base could be very difficult to manage. Ponder if there's any nice workaround for this.
+
+## Toolkits & refs
+
+For footnotes, it would be nice to have a toolkit popping up. If each toolkit is also a zettel, this wouldn't be a problem. Moreover, it should allow you to both show it at the bottom of the page and as a toolkit.
+
+Also, we could have a component which is a link/ref to a zettel (1. to show in a different page, 2. to show in the same page) or even show a peep of the zettel as a toolkit (maybe instead of abstract, peep is a better name). This is nice recently defined objects could be peeped when hovering over. Maybe a little logic would be necessary to parse normal text into this format.
+
+## Revision history
+
+Implement a way to review different versions of each zettel
+
+(this will be postponed to a better time)
+
+
--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,20 +1,72 @@
 import Link from "next/link";
 
 import { filterZettels, zettelById } from "zettel/lib/retrieve";
+import { getZettelVersions, getZettelAtVersion, getZettelDiff } from "zettel/lib/versions";
 import Zettel from "zettel/ui/z/zettel";
+import VersionSelector from "zettel/ui/components/version_selector";
+import DiffViewer from "zettel/ui/components/diff_viewer";
 
 export default async function Page({ 
-  params 
+  params,
+  searchParams
 }: { 
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
+  searchParams?: Promise<{ version?: string; diff?: string; from?: string; to?: string }>;
 }) {
   const { id } = await params;
-  const zettel = await zettelById(id);
+  const search = await searchParams;
+  const versionParam = search?.version ? parseInt(search.version) : undefined;
+  const diffMode = search?.diff === "1";
+  const fromRevision = search?.from ? parseInt(search.from) : undefined;
+  const toRevision = search?.to ? parseInt(search.to) : undefined;
+
+  const versions = await getZettelVersions(id);
+
+  let zettel;
+  let diff: string | null = null;
+
+  if (diffMode && fromRevision && toRevision) {
+    diff = await getZettelDiff(id, fromRevision, toRevision);
+  } else if (versionParam) {
+    zettel = await getZettelAtVersion(id, versionParam);
+  } else {
+    zettel = await zettelById(id);
+  }
+
+  if (!zettel && !diff) {
+    return (
+      <div>
+        <Link href="/notes" className='text-gray-500'>Back to notes</Link>
+        <p>Zettel not found</p>
+      </div>
+    );
+  }
 
   return (
     <>
       <Link href="/notes" className='text-gray-500'>Back to notes</Link>
-      <Zettel key={id} {...zettel} mode="full" showTitle={true} showDate={true} showTags={true} showAbstract={true} />
+      {versions.length > 0 && (
+        <VersionSelector
+          zettelId={id}
+          versions={versions}
+          currentRevision={versionParam}
+          currentDiff={diffMode && fromRevision && toRevision ? { from: fromRevision, to: toRevision } : undefined}
+        />
+      )}
+      {diffMode && diff && fromRevision && toRevision ? (
+        <DiffViewer diff={diff} fromRevision={fromRevision} toRevision={toRevision} />
+      ) : (
+        <Zettel
+          key={id}
+          {...zettel}
+          mode="full"
+          showTitle={true}
+          showDate={true}
+          showTags={true}
+          showAbstract={true}
+          version={versionParam ? versions.find(v => v.revision === versionParam) : undefined}
+        />
+      )}
     </>
   );
 }
